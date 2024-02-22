49e6f6fa7a4c9743dd0f90591988ff866cef2ecc
Artem <dogmaster31072003@gmail.com>
2024-02-22 22:56:32 +0200

    Initial Commit

---
+++ Films/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Films
+//
+//  Created by Melkor on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
