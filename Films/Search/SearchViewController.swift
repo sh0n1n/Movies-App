8690dffe75d862650c72732526f7515266d7e93b
Artem <dogmaster31072003@gmail.com>
2024-02-22 23:23:48 +0200

    Added GUI-Variables for search

--- Films/Search/SearchViewController.swift
+++ Films/Search/SearchViewController.swift
@@ -3,5 +3,23 @@
 //  Films
 //
 
+import UIKit
 
-import Foundation
+final class SearchViewController: UIViewController {
+    //MARK: - GUI Variables
+    private lazy var collectionViewSearch: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private lazy var seachBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Search something"
+         return searchBar
+     }()
+    
+    
+}
